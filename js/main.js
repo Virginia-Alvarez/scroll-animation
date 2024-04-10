9fb38373c5ad89a85fc81f1c08c272e9866a9d06
Virginia Álvarez <virginiaalvarez.ar@gmail.com>
2024-04-10 15:51:08 +0200

    add js and css's animation

--- js/main.js
+++ js/main.js
@@ -2,10 +2,20 @@
 
 const boxes = document.querySelectorAll('.box');
 
-window.addEventListener('scroll',()=>{
+window.addEventListener('scroll', scroller);
 
-});
+scroller();
 
 function scroller(){
-    
+    const heigtWindow= window.innerHeight / 5 * 4
+
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top
+            if(boxTop < heigtWindow){
+                box.classList.add('show');
+            }else{
+                box.classList.remove('show');
+            }
+    })
 }
+
